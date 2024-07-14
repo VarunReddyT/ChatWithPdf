83c0f5089152888f45d20de3f5cf82c9535ab8d9
Varun-MSI <tvarun2014@gmail.com>
2024-07-14 21:06:08 +0530

    New

--- flask/main.py
+++ flask/main.py
@@ -74,4 +74,6 @@ def ask():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    
